0ee8140ddb1e6c0591d4696f7bb43817e29903df
Aditya <meadisingh12@gmail.com>
2024-08-06 11:33:22 +0530

    added navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import StickyNavbar from './components/common/Navbar'
+import { FooterWithLogo } from './components/common/Footer'
 import Products from './pages/Products'
 import ProductForm from './pages/productDesForm'
 import SignUpForm from './pages/signUpForm'
@@ -12,17 +13,15 @@ function App() {
 	return (
 		<>
 			<StickyNavbar />
-
-			<Router>
-				<div>
-					<Routes>
-						<Route path="/" element={<Products />} />
-						<Route path="/product-form" element={<ProductForm />} />
-						<Route path="/signup-form" element={<SignUpForm />} />
-						<Route path="/accounts" element={<Accounts />} />
-					</Routes>
-				</div>
-			</Router>
+			<div>
+				<Routes>
+					<Route path="/" element={<Products />} />
+					<Route path="/product-form" element={<ProductForm />} />
+					<Route path="/signup-form" element={<SignUpForm />} />
+					<Route path="/accounts" element={<Accounts />} />
+				</Routes>
+			</div>
+			<FooterWithLogo />
 		</>
 	)
 }
